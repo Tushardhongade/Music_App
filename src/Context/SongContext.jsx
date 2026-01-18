afeba4b7d67b2c440a1a206c720486ee363aca89
Tushar Dhongade <109501445+Tushardhongade@users.noreply.github.com>
2026-01-18 17:45:49 +0530

    Update backend URL for production environment

--- src/Context/SongContext.jsx
+++ src/Context/SongContext.jsx
@@ -7,7 +7,7 @@ export const SongContextState = ({ children }) => {
   if(document.domain === "localhost"){
     __URL__ = "http://localhost:1337"
   }else{
-    __URL__ = "https://music-player-app-backend-yq0c.onrender.com"
+    __URL__ = "https://music-app-backendp.vercel.app/"
   }
   const audio = new Audio();
   const song = {
